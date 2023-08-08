bf2143645279f3a80201fd80ba7b828b57e69a1d
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2023-08-08 02:00:17 -0900

    hw2 added

---
+++ src/homework/hw2/Main.java
@@ -0,0 +1,72 @@
+package homework.hw2;
+
+class sList {
+    Node head;
+    Node tail;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+    }
+
+    void push_front(int value){
+        Node node = new Node();
+        node.value = value;
+
+        if(head == null){
+            tail = node;
+        }else {
+            node.next = head;
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    public void revert() {
+        Node cur = head;
+
+        while (cur != null) {
+            Node next = cur.next;
+            Node prev = cur.prev;
+            cur.next = prev;
+            cur.prev = next;
+            if (prev == null) {
+                tail = cur;
+            }
+            if (next == null) {
+                head = cur;
+            }
+            cur = next;
+        }
+
+    }
+
+    void print(){
+        sList.Node cur = head;
+        while(cur != null){
+            System.out.println(cur.value);
+            cur = cur.next;
+        }
+    }
+
+}
+public class Main {
+
+    public static void main(String[] args) {
+
+        sList list = new sList();
+        for(int i=0; i<=10; i++) {
+            list.push_front(i);
+        }
+
+        list.print();
+        System.out.println();
+
+        list.revert();
+
+        list.print();
+
+    }
+
+}
